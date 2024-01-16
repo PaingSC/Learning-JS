edfd4cc91135ecc20969c4f889597b49b3b576aa
PaingSC <paingsoechit.job@gmail.com>
2024-01-16 21:16:09 +0630

    function-methods: call() & apply()

--- A_Closer_Look_at_Functions/script.js
+++ A_Closer_Look_at_Functions/script.js
@@ -39,3 +39,47 @@ console.log(greeter);
 
 greeter("Paing");
 greet("Hello")("Thae");
+// ------------------------------------------------------
+
+// ------------------------------------------------------
+// Function Methods: call & apply methods
+const lufthansa = {
+  airline: "Lufthanasa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}.`
+    );
+    this.bookings.push({
+      flight: `${this.airline}${this.flightNum}`,
+      name,
+    });
+  },
+};
+
+lufthansa.book(239, "Paing Soe Chit");
+
+// Same as the lufthanasa object without book() method
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+console.log(lufthansa);
+
+// Does not work
+// book(239, "Paing Soe Chit");
+
+// The call method of functions
+book.call(eurowings, 27, "Thae Lay"); // the first argument is the object that 'this' keyword will be defined!
+book.call(lufthansa, 239, "Thae Lay");
+console.log(eurowings);
+
+// The apply method of functions
+const flightData = [358, "Win Oo"];
+book.apply(eurowings, flightData); //The apply method accept the second argument as array
+
+book.call(eurowings, ...flightData); // Same as the above apply method
